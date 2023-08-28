d0a8ef35c63ce1fe8ef219921cc6983b3e8b79fc
thorcamara <thorcamaraalthoff@gmail.com>
2023-08-28 11:18:01 -0300

    Added constants displayResult and startTest

--- script.js
+++ script.js
@@ -66,3 +66,33 @@ const timeReduce = () => {
   time = 60;
   timer = setInterval(updateTimer, 1000);
 };
+
+const displayResult = () => {
+  document.querySelector(".result").style.display = "block";
+  clearInterval(timer);
+  document.getElementById("stop-test").style.display = "none";
+  userInput.disabled = true;
+  let timeTaken = 1;
+  if (time != 0) {
+    timeTaken = (60 - time) / 100;
+  }
+  document.getElementById("wpm").innerText = (userInput.value.length / 5 / timeTaken).toFixed(2) + "wpm";
+  document.getElementById("accuracy").innerText = Math.round(((userInput.value.length - mistakes) / userInput.value.length) * 100) + "%";
+};
+
+const startTest = () => {
+  mistakes = 0;
+  timer = "";
+  userInput.disabled = false;
+  timeReduce();
+  document.getElementById("start-test").style.display = "none";
+  document.getElementById("stop-test").style.display = "block";
+};
+
+window.onload = () => {
+  userInput.value = "";
+  document.getElementById("start-test").style.display = "block";
+  document.getElementById("stop-test").style.display = "none";
+  userInput.disabled = true;
+  renderNewQuote();
+}
