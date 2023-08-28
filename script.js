fb299a829ed68f218d73faf594d647c90861d007
thorcamara <thorcamaraalthoff@gmail.com>
2023-08-27 22:50:44 -0300

    Added constants and variables

---
+++ script.js
@@ -0,0 +1,19 @@
+const quoteApiUrl = "https://api.quotable.io/random?minLength=80&maxLength=100";
+const quoteSection = document.getElementById("quote");
+const userInput = document.getElementById("quote-input");
+
+let quote = "";
+let time = 60;
+let timer = "";
+let mistakes = 0;
+
+const renderNewQuote = async () => {
+  const response = await fetch(quoteApiUrl);
+  let data = await response.json();
+  quote = data.content;
+
+  let arr = quote.split("").map((value) => {
+    return "<span class='quote-chars'>" + value + "</span>";
+  });
+  quoteSection.innerHTML += arr.join("");
+};
